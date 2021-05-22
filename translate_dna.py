810923eb91526a9a2e7b172fe70122a1b43df674
msim <56232384+mrosim@users.noreply.github.com>
2021-05-23 00:06:52 +0200

    Update translate_dna.py

--- translate_dna.py
+++ translate_dna.py
@@ -2,16 +2,15 @@ def translate(seq,frame):
     BASES = 'ACGT'
     # standard code translation
     AA = 'KNKNTTTTRSRSIIMIQHQHPPPPRRRRLLLLEDEDAAAAGGGGVVVV*Y*YSSSS*CWCLFLF'
-    # transform DNA sequence in numbers A=0; C=1; G=2; T=3
+    # transform DNA sequence in numbers: A=0; C=1; G=2; T=3
     seqn = [str(BASES.find(i)) for i in seq.upper()]
     # list of all codons in all forward frames
     allframes = [seqn[n:n+3] for n,j in enumerate(seqn) if n <= (len(seq)-3)]
-    # list of codons in the given frame
-    translated = [allframes[k] for k in range(frame,len(allframes),3)]
-    # translate the codons taking the aa in AA at position given by the number(in base 4)
-    return ''.join([AA[int(i,4)] for i in [''.join(i) for i in translated]])
+    # translate the codons at the given frame taking the aa in AA string at position given by the number (in base 4) in allframes list
+    return ''.join([AA[int(i,4)] for i in [''.join(i) for i in allframes[frame::3]]])
 
 
 if __name__ == '__main__':
-    print(translate('ggggcagctgggaggg',1))
-
+    query_seq = input('seq DNA: ')
+    for k in range(3):
+        print('frame {}: '.format(k+1), translate(query_seq,k))
